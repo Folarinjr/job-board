42593e24993bdf8f06cce326780b75fd1274363d
Folarinjr <folarinademola2@gmail.com>
2023-03-28 03:02:17 +0100

    Custom font logic updated

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, FOLARIN</Text>
+        <Text style={styles.welcomeMessage}>Find your dream job!</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
